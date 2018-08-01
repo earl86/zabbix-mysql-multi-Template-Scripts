19725552dd98eed4bbf1f9cfc7cacdc26beffcb4
earl86 <earl86@users.noreply.github.com>
2018-08-01 14:52:22 +0800

    Add files via upload

--- scripts/get_mysql_stats_wrapper.sh
+++ scripts/get_mysql_stats_wrapper.sh
@@ -3,27 +3,30 @@
 # It runs the script every 5 min. and parses the cache file on each following run.
 # Authors: earl86
 
+USERNAME=zabbix
+PASSWORD=zabbix
+
 SERVICEHOST=$1
 SERVICEPORT=$2
 ITEM=$3
-USERNAME=zabbix
-PASSWORD=zabbix2018
+
 DIR=`dirname $0`
+
 CMD="/usr/bin/python $DIR/get_mysql_stats.py --servicehost $SERVICEHOST --serviceport $SERVICEPORT --username $USERNAME--password $PASSWORD"
 CACHEFILE="/tmp/$SERVICEHOST-$SERVICEPORT-mysql_zabbix_stats.txt"
 
-if [ "$ITEM" = "running_slave" ]; then
-    # Check for running slave
-    RES=`HOME=~zabbix mysql -h $SERVICEHOST -P $SERVICEPORT -u$USERNAME -p$PASSWORD -e 'SHOW SLAVE STATUS\G' | egrep '(Slave_IO_Running|Slave_SQL_Running):' | awk -F: '{print $2}' | tr '\n' ','`
-    if [ "$RES" = " Yes, Yes," ]; then
+if [ "$ITEM" = "mysqld_alive" ]; then
+    RES=`HOME=~zabbix mysql -h $SERVICEHOST -P $SERVICEPORT -u$USERNAME -p$PASSWORD -N -e 'select 1 from dual;' 2>/dev/null`
+    if [ "$RES" = "1" ]; then
         echo 1
     else
         echo 0
     fi
     exit
-elif [ "$ITEM" = "mysqld_alive" ]; then
-    RES=`HOME=~zabbix mysql -h $SERVICEHOST -P $SERVICEPORT -u$USERNAME -p$PASSWORD -N -e 'select 1 from dual;' 2>/dev/null`
-    if [ "$RES" = "1" ]; then
+elif [ "$ITEM" = "slave_running" ]; then
+    # Check for running slave
+    RES=`HOME=~zabbix mysql -h $SERVICEHOST -P $SERVICEPORT -u$USERNAME -p$PASSWORD -e 'SHOW SLAVE STATUS\G' | egrep '(Slave_IO_Running|Slave_SQL_Running):' | awk -F: '{print $2}' | tr '\n' ','`
+    if [ "$RES" = " Yes, Yes," ]; then
         echo 1
     else
         echo 0
